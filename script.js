5eb5b30c0d6c6503b060f4053706c54f8e724606
MarceloMosl <marcelomosl@icloud.com>
2022-10-29 17:10:00 -0300

    Projeto OK - Faltando Bonus

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let statusReturned;
 let trocaFundo;
 let backGround;
 let userValue;
+let usuarioClicado = "Todos";
+let documento;
+let usersNames;
+let toWhom = "message";
+function sendToAll() {
+    usuarioClicado = "Todos"
+}
 function login() {
     const nameUser = prompt("Qual seu nome?")
     userValue = { name: nameUser }
@@ -33,32 +40,28 @@ function incomeMessages(messages) {
     chat1 = document.querySelector(".chatoul");
     chat1.innerHTML = ""
     for (i = 0; i < messageContent.length; i++) {
-        if (messageContent[i].type !== "status") {
-            chat1.innerHTML += `<li class="msgs"> <div class="content">
-    <span class="time"> (${messageContent[i].time}) </span>
-    <span class="nome"> ${messageContent[i].from} para ${messageContent[i].to} :</span>
-    <span class="text"> ${messageContent[i].text}  </span></div> </li>`
+        if (messageContent[i].type === "message" || messageContent[i].type === "private_message" && messageContent[i].to === userValue.name || messageContent[i].type === "private_message" && messageContent[i].from === userValue.name){
+            if(messageContent[i].type === "private_message"){
+                chat1.innerHTML += `<li class="msgs priveteMessage"> <div class="content">
+            <span class="time"> (${messageContent[i].time}) </span>
+            <span class="nome"> ${messageContent[i].from} para ${messageContent[i].to} :</span>
+            <span class="text"> ${messageContent[i].text}  </span></div> </li>`
+
+            }else{
+                chat1.innerHTML += `<li class="msgs"> <div class="content">
+            <span class="time"> (${messageContent[i].time}) </span>
+            <span class="nome"> ${messageContent[i].from} para ${messageContent[i].to} :</span>
+            <span class="text"> ${messageContent[i].text}  </span></div> </li>`}
+        } else if (messageContent[i].type == "status") {
+            chat1.innerHTML += `<li class="msgs login"> <div class="content">
+            <span class="time"> (${messageContent[i].time}) </span>
+            <span class="nome"> ${messageContent[i].from}:</span>
+            <span class="text"> ${messageContent[i].text}  </span></div> </li>`
         } else {
-            chat1.innerHTML += `<li class="msgs"> <div class="content">
-    <span class="time"> (${messageContent[i].time}) </span>
-    <span class="nome"> ${messageContent[i].from}:</span>
-    <span class="text"> ${messageContent[i].text}  </span></div> </li>`
         }
         lastTxt();
     }
-    changeBackground(messageContent);
-    setTimeout(getMessages, 3000)
-}
-function changeBackground(data) {
-    trocaFundo = Array.from(document.querySelectorAll(".msgs"))
-    for (i = 0; i < data.length; i++) {
-        if (data[i].type == "status") {
-            trocaFundo[i].classList.add("login")
-        }
-        else if (data[i].type == "private_message") {
-            trocaFundo[i].classList.add("priveteMessage")
-        }
-    }
+   setTimeout(getMessages, 3000)
 }
 function lastTxt() {
     lastMessage = Array.from(document.querySelectorAll(".text"))
@@ -75,47 +78,69 @@ function ativoStatus(status) {
 }
 function erroStatus(status) {
     console.log("status: offline")
-    window.location.reload();
 }
 function sendMsg() {
     textMsg = document.querySelector(".input")
-    sentMsg = { from: userValue.name, to: "Todos", text: textMsg.value, type: "message", }
+    sentMsg = { from: userValue.name, to: usuarioClicado, text: textMsg.value, type: toWhom }
 
     let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", sentMsg)
     textMsg.value = ""
+    usuarioClicado = "Todos"
     promise.then(sentLog)
     promise.catch(errorSend)
 }
 function sentLog(response) {
-    console.log("FOI CARLHO")
+    console.log("mensagem enviada")
+    getMessages();
 }
 function errorSend(response) {
-    console.log(response)
-    console.log("oi")
-    window.location.onload()
+    console.log("Erro ao enviar a Mensagem")
+    window.location.reload()
 }
 function getParticipants() {
     promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
     promise.then(displayParticipants)
     promise.catch(errorParticipants)
-} let aabb;
-function displayParticipants(response){
-    aabb = response.data
+}
+function displayParticipants(response) {
+    usersNames = response.data
     participantsHTML = document.querySelector(".users");
     participantsHTML.innerHTML = ""
-    for (i = 0; i < aabb.length; i++) {
-        participantsHTML.innerHTML += `<div class="usuarios" onclick="getUserSide(this)">
+    for (i = 0; i < usersNames.length; i++) {
+        participantsHTML.innerHTML += `
+    <div class="options-sidebar" onclick="getUserSide(this)">
+        <div class="participants">
             <ion-icon name="people"></ion-icon>
-            <p>${aabb[i].name}</p>
-            </div>`
+            <p>${usersNames[i].name}</p>
+        </div>
+        <div class="icon-check">
+            <ion-icon id="ion-icon" name="checkmark-circle"></ion-icon>
+        </div>
+    </div>`
     }
-    setInterval(getParticipants, 5000)
+    setInterval(getParticipants, 10000)
 }
 function errorParticipants(response) {
-    console.log(response)
-    console.log("error")
-
+    window.location.onload()
 }
 function getUserSide(element) {
-    console.log(element)
+    usuarioClicado = element.children[0]
+    usuarioClicado = usuarioClicado.children[1].innerHTML
+    userClickedSidebar(element);
+}
+function userClickedSidebar(clicked) {
+    selected = document.querySelectorAll(".options-sidebar")
+    for (i = 0; i < selected.length - 1; i++) {
+        if (selected[i].classList.contains("back") === true) {
+            selected[i].classList.remove("back")
+        } else {
+            clicked.classList.add("back")
+        }
+    }
+}
+function sendToAll(){
+    toWhom = "message"
+}
+function sendPrivate(){
+    toWhom = "private_message"
 }
